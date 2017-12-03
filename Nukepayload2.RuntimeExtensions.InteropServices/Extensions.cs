532fc93ba97a8c59d9060066a8ab6ee30682c475
Nukepayload2 <1939357182@qq.com>
2017-12-03 17:42:42 +0800

    添加项目文件。

---
+++ Nukepayload2.RuntimeExtensions.InteropServices/Extensions.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Nukepayload2.RuntimeExtensions.InteropServices
+{
+    /// <summary>
+    /// Make Visual Basic codes accessible to unsafe members of runtime types. This standard module is designed for Visual Basic and not intended to use in C# or F#.
+    /// </summary>
+    [StandardModule, HideModuleName]
+    public static class Extensions
+    {
+        /// <summary>
+        /// Gets the pointer that points to to the current position.
+        /// </summary>
+        public static unsafe IntPtr DangerousGetPositionPointer(this UnmanagedMemoryStream strm)
+        {
+            return new IntPtr(strm.PositionPointer);
+        }
+        /// <summary>
+        /// Sets the pointer that points to to the current position.
+        /// </summary>
+        public static unsafe void DangerousSetPositionPointer(this UnmanagedMemoryStream strm, IntPtr ptr)
+        {
+            strm.PositionPointer = (byte*)ptr;
+        }
+        /// <summary>
+        /// Appends characters from the specified address and count.
+        /// </summary>
+        public static unsafe StringBuilder DangerousAppend(this StringBuilder sb, IntPtr chrPtr, int charCount)
+        {
+            return sb.Append((char*)chrPtr, charCount);
+        }
+        /// <summary>
+        /// Calculates the length of decoded buffer in bytes.
+        /// </summary>
+        public static unsafe int GetByteCount(this Encoding encoding, IntPtr chrPtr, int charCount)
+        {
+            return encoding.GetByteCount((char*)chrPtr, charCount);
+        }
+        /// <summary>
+        /// Decodes characters in the specified buffer to another buffer.
+        /// </summary>
+        public static unsafe int GetBytes(this Encoding encoding, IntPtr chrPtr, int charCount, IntPtr outBuffer, int outBufferLength)
+        {
+            return encoding.GetBytes((char*)chrPtr, charCount, (byte*)outBuffer, outBufferLength);
+        }
+        /// <summary>
+        /// Calculates the count of encoded characters.
+        /// </summary>
+        public static unsafe int GetCharCount(this Encoding encoding, IntPtr ptr, int length)
+        {
+            return encoding.GetCharCount((byte*)ptr, length);
+        }
+        /// <summary>
+        /// Encodes raw bytes in the specified buffer to a buffer of characters.
+        /// </summary>
+        public static unsafe int GetChars(this Encoding encoding, IntPtr ptr, int length, IntPtr outChars, int outCharsCount)
+        {
+            return encoding.GetChars((byte*)ptr, length, (char*)outChars, outCharsCount);
+        }
+    }
+}
---
+++ Nukepayload2.RuntimeExtensions.InteropServices/Marshal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Security;
+
+namespace Nukepayload2.RuntimeExtensions.InteropServices
+{
+    /// <summary>
+    /// Provides helper methods for creating objects with unsafe manner. This class is designed for Visual Basic and not intended to use in C# or F#.
+    /// </summary>
+    public static class Marshal
+    {
+        /// <summary>
+        /// Creates a <see cref="Span{T}"/> from unmanaged memory.
+        /// </summary>
+        public static unsafe Span<T> UnsafeCreateSpan<T>(IntPtr ptr, int length)
+        {
+            return new Span<T>((void*)ptr, length);
+        }
+        /// <summary>
+        /// Creates a <see cref="ReadOnlySpan{T}"/> from unmanaged memory.
+        /// </summary>
+        public static unsafe ReadOnlySpan<T> UnsafeCreateReadOnlySpan<T>(IntPtr ptr, int length)
+        {
+            return new ReadOnlySpan<T>((void*)ptr, length);
+        }
+        /// <summary>
+        /// Creates a <see cref="UnmanagedMemoryStream"/> from unmanaged memory.
+        /// </summary>
+        public static unsafe UnmanagedMemoryStream UnsafeCreateUnmanagedMemoryStream(IntPtr ptr, long length)
+        {
+            return new UnmanagedMemoryStream((byte*)ptr, length);
+        }
+        /// <summary>
+        /// Creates a <see cref="UnmanagedMemoryStream"/> from unmanaged memory.
+        /// </summary>
+        public static unsafe UnmanagedMemoryStream UnsafeCreateUnmanagedMemoryStream(IntPtr ptr, long length, long capacity, FileAccess access)
+        {
+            return new UnmanagedMemoryStream((byte*)ptr, length, capacity, access);
+        }
+        /// <summary>
+        /// Creates a <see cref="string"/> from unmanaged memory.
+        /// </summary>
+        public static unsafe string UnsafeCreateNullTerminatedString(IntPtr chrPtr)
+        {
+            return new string((char*)chrPtr);
+        }
+        /// <summary>
+        /// Creates a <see cref="string"/> from unmanaged memory.
+        /// </summary>
+        public static unsafe string UnsafeCreateString(IntPtr chrPtr, int startIndex, int charCount)
+        {
+            return new string((char*)chrPtr, startIndex, charCount);
+        }
+        /// <summary>
+        /// Creates a <see cref="SecureString"/> from unmanaged memory.
+        /// </summary>
+        public static unsafe SecureString UnsafeCreateSecureString(IntPtr chrPtr, int charCount)
+        {
+            return new SecureString((char*)chrPtr, charCount);
+        }
+    }
+}
